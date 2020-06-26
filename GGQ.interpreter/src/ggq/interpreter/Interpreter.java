a70d8d13d93a88b19836a7564ac380fc41c8a7a8
Simon Knott <info@simonknott.de>
2020-06-26 21:35:15 +0200

    Add testing code

--- GGQ.interpreter/src/ggq/interpreter/Interpreter.java
+++ GGQ.interpreter/src/ggq/interpreter/Interpreter.java
@@ -1,9 +1,6 @@
 package ggq.interpreter;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -12,44 +9,6 @@ import org.eclipse.emf.ecore.*;
 import ggq.gql.*;
 
 public class Interpreter {
-	
-	private List<Iterable<Edge>> matches = new ArrayList<>();
-	
-	void emitMatch(Iterable<Edge> match) {
-		matches.add(match);
-	}
-	
-	boolean checkEdge(EReference type, Iterable<Edge> match, GraphQuery hostGraph) {
-		return false;
-	}
-	
-	List<Vertex> getCandidateNodes(EClass type, GraphQuery hostGraph) {
-		return Collections.emptyList();
-	}
-	
-	Iterable<Edge> addMapping(Iterable<Edge> m, Vertex n, Vertex v, GraphQuery h) {
-		return Collections.emptyList();
-	}
-	
-	void extendMatch(int i, Iterable<Edge> m, GraphQuery q, GraphQuery h) {
-		if (i == q.getContainedVertices().size()) {
-			boolean valid = true;
-			for (Edge e : q.getContainedEdges()) {
-				valid = valid && checkEdge(e.getType(), m, h);
-			}
-			
-			if (valid) {
-				emitMatch(m);
-			}
-		}
-		
-		Vertex n = q.getContainedVertices().get(i);
-		Iterable<Vertex> V = getCandidateNodes(n.getType(), h);
-		for (Vertex v : V) {
-			Iterable<Edge> newM = addMapping(m, n, v, h);
-			extendMatch(i + 1, newM, q, h);
-		}
-	}
 	
 	private Stream<Map<Vertex, EObject>> generatePossibleMappings(Collection<EObject> host, GraphQuery query) {
 		return Stream.empty();
--- GGQ.interpreter/src/ggq/interpreter/Tester.java
+++ GGQ.interpreter/src/ggq/interpreter/Tester.java
@@ -1,28 +1,39 @@
 package ggq.interpreter;
 
-import ggq.gql.Edge;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.eclipse.emf.ecore.EObject;
+
 import ggq.gql.GraphQuery;
+import ggq.gql.ModelingFactory;
+import ggq.gql.Vertex;
 
 public class Tester {
-	private static GraphQuery createHostGraph() {
-		
+	
+	private static Collection<EObject> createHostGraph() {
+		return Collections.emptyList();
 	}
 	
 	private static GraphQuery createQueryGraph() {
-		
+		ModelingFactory factory = ModelingFactory.eINSTANCE;
+		return factory.createGraphQuery();
 	}
 	
 	public static void main(String... args) {
 		Interpreter i = new Interpreter();
-		Iterable<Iterable<Edge>> matches = i.match(createHostGraph(), createQueryGraph());
-		for (Iterable<Edge> match : matches) {
+		Stream<Map<Vertex, EObject>> matches = i.match(createHostGraph(), createQueryGraph());
+		matches.forEach(match -> {
 			System.out.print("{");
 			
-			for (Edge edge : match) {
-				System.out.print("(" + edge.getSource().getId() + ", " + edge.getTarget().getId() + "), ");	
+			for (Map.Entry<Vertex, EObject> entry : match.entrySet()) {
+				
+				System.out.print("(" + entry.getKey().getId() + ", " + entry.getValue() + "), ");	
 			}
 			
 			System.out.print("}\n");
-		}
+		});
 	}
 }
