ac2d5001f1f0cbfa8476e4187301f3c110adcc92
Simon Knott <info@simonknott.de>
2020-06-27 15:01:56 +0200

    Implement first working version of interpreter 🎉

--- GGQ.interpreter/src/ggq/interpreter/Interpreter.java
+++ GGQ.interpreter/src/ggq/interpreter/Interpreter.java
@@ -1,7 +1,12 @@
 package ggq.interpreter;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.eclipse.emf.ecore.*;
@@ -10,16 +15,52 @@ import ggq.gql.*;
 
 public class Interpreter {
 	
-	private Stream<Map<Vertex, EObject>> generatePossibleMappings(Collection<EObject> host, GraphQuery query) {
-		return Stream.empty();
+	static private <T> Collection<T> copyWithoutObject(Collection<T> collection, T without) {
+		return collection.stream().filter(obj -> obj != without).collect(Collectors.toList());
 	}
 	
-	private boolean isMappingAMatch(Map<Vertex, EObject> mapping, Collection<EObject> host, GraphQuery query) {
-		return false;
+	static private Stream<Map<Vertex, EObject>> generatePossibleMappings(Collection<EObject> host, GraphQuery query, int queryNodesIndex) {
+		if (queryNodesIndex >= query.getContainedVertices().size()) {
+			return Stream.of(new HashMap<>());
+		}
+		
+		Vertex node = query.getContainedVertices().get(queryNodesIndex);
+		return host.stream().flatMap(eobject -> {
+			if (eobject.eClass() != node.getType()) {
+				return Stream.empty();
+			}
+			
+			Collection<EObject> hostWithoutThisEObject = copyWithoutObject(host, eobject); 
+			Stream<Map<Vertex, EObject>> mappingsForRestOfQuery = generatePossibleMappings(hostWithoutThisEObject, query, queryNodesIndex + 1);
+			return mappingsForRestOfQuery.map(mapping -> {
+				mapping.put(node, eobject);
+				return mapping;
+			});
+		});
 	}
 	
-	Stream<Map<Vertex, EObject>> match(Collection<EObject> host, GraphQuery query) {
-		return generatePossibleMappings(host, query).filter(mapping -> isMappingAMatch(mapping, host, query));
+	static private Stream<Map<Vertex, EObject>> generatePossibleMappings(Collection<EObject> host, GraphQuery query) {
+		return generatePossibleMappings(host, query, 0);
+	}
+	
+	static private boolean isMappingAMatch(Map<Vertex, EObject> mapping, Collection<Edge> queryEdges) {
+		return queryEdges.stream().allMatch(edge-> {
+			Vertex source = edge.getSource();
+			Vertex target = edge.getTarget();
+			EReference type = edge.getType();
+			
+			EObject mappedSource = mapping.get(source);
+			EObject mappedTarget = mapping.get(target);
+			
+			List<EObject> referencedTargets = (List<EObject>) mappedSource.eGet(type);
+			boolean thereIsAMatchingRefInHostGraph = referencedTargets.contains(mappedTarget);
+			
+			return thereIsAMatchingRefInHostGraph;
+		});
+	}
+	
+	static Stream<Map<Vertex, EObject>> match(Collection<EObject> host, GraphQuery query) {
+		return generatePossibleMappings(host, query).filter(mapping -> isMappingAMatch(mapping, query.getContainedEdges()));
 	}
 	
 }
